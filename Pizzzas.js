b12aeb067db6249814d63f6ffde3797c02f4a9e9
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 10:49:37 +0500

    Add files via upload

---
+++ Pizzzas.js
@@ -0,0 +1,9 @@
+"use strict";
+// Array of favorite pizza names
+let favorite_pizzas = ['Pepperoni', 'Margherita', 'BBQ Chicken'];
+// Loop through the array and print a sentence for each pizza
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+// Additional line stating how much you like pizza
+console.log("Pizza is one of my favorite foods, and I really love it!");
---
+++ Pizzzas.ts
@@ -0,0 +1,10 @@
+// Array of favorite pizza names
+let favorite_pizzas: string[] = ['Pepperoni', 'Margherita', 'BBQ Chicken'];
+
+// Loop through the array and print a sentence for each pizza
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+// Additional line stating how much you like pizza
+console.log("Pizza is one of my favorite foods, and I really love it!");
